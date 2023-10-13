b1b6374e656fe43700a44e26cf9e1a17ea8d24c6
JangamRuthvik <jruthvik7171@gmail.com>
2023-10-13 10:33:23 +0530

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
-import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/tabs.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MaterialApp(
     home: Tabs(),
   ));
