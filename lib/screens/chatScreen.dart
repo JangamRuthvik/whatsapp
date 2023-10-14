5add25561391be8b2289721625662a9439ac339f
JangamRuthvik <jruthvik7171@gmail.com>
2023-10-15 01:05:07 +0530

    keyboard

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -47,53 +47,110 @@ class ChatScreen extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          child: Stack(children: [
-            Positioned(bottom: 0, child: MessageBar())
-            // MessageBar
-          ]),
+          child: Stack(children: [Positioned(bottom: 0, child: MessageBar())]),
         ));
   }
 }
 
-class MessageBar extends StatelessWidget {
+class MessageBar extends StatefulWidget {
+  @override
+  State<MessageBar> createState() => _MessageBarState();
+}
+
+class _MessageBarState extends State<MessageBar> {
+  bool ischanged = false;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          margin: EdgeInsets.only(left: 5, right: 1),
-          decoration: ShapeDecoration(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32))),
-          width: MediaQuery.of(context).size.width * 8.4 / 10,
-          child: Row(
+    return  Row(
+        children: [
+          Stack(
             children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.emoji_emotions)),
-              Container(
-                width: 100,
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: 'Message'),
-                ),
+              SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 5, right: 1),
+                    decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                    width: MediaQuery.of(context).size.width * 8.4 / 10,
+                    child: Stack(children: [
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.emoji_emotions)),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 40),
+                        child: SizedBox(
+                          width: ischanged ? 200 : 150,
+                          child: TextField(
+                            onChanged: (value) {
+                              setState(() {
+                                if (value.isEmpty) {
+                                  ischanged = false;
+                                } else {
+                                  ischanged = true;
+                                }
+                              });
+                            },
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 6,
+                            minLines: 1,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, hintText: 'Message'),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                          child: Row(
+                        children: [
+                          if (!ischanged)
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.file_present_outlined)),
+                          if (!ischanged)
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.currency_rupee)),
+                          if (!ischanged)
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.camera_alt)),
+                          if (ischanged)
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.file_present_outlined))
+                        ],
+                      ))
+                    ]),
+                  ),
+                ],
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.file_present_outlined)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.currency_rupee)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt)),
-            ],
+            ),
+            Positioned(
+              bottom: 0,
+              right: 2,
+              child: InkWell(
+                      child: Container(
+                          // margin: EdgeInsets.only(left: 1, right: 2),
+                          // width: MediaQuery.of(context).size.width*0.2,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromARGB(204, 7, 94, 84)),
+                          child: Center(
+                              child: ischanged?IconButton(
+                                  onPressed: () {}, icon: Icon(Icons.send)):
+                                  IconButton(
+                                  onPressed: () {}, icon: Icon(Icons.mic))))),
+            )
+            ]
           ),
-        ),
-        InkWell(
-            child: Container(
-                margin: EdgeInsets.only(left: 1, right: 2),
-                // width: MediaQuery.of(context).size.width*0.2,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(204, 7, 94, 84)),
-                child: Center(
-                    child:
-                        IconButton(onPressed: () {}, icon: Icon(Icons.mic)))))
-      ],
+        ],
     );
   }
 }
